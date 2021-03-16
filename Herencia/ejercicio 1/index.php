2dca13e81b9ca2c9024a9c0f9a48ea95c9fa3595
ankaroalsy <80726497+ankaroalsy@users.noreply.github.com>
2021-03-15 21:03:23 -0400

    Update index.php

--- Herencia/ejercicio 1/index.php
+++ Herencia/ejercicio 1/index.php
@@ -1,3 +1,13 @@
+/*Una agencia vendedora de autos desea un sistema computacional para administrar
+Los datos de sus vehículos y clasificarlos por tipo. Todos los autos tienen los 
+siguientes datos: número de serie del motor, placa, color, marca, año, precio.
+Los vehículos se clasifican en autos, camionetas y vagonetas. Para los autos y 
+vagonetas, también es importante almacenar la cantidad de pasajeros mientras que
+para las camionetas Se deben controlar la capacidad de carga en kilogramos y la 
+cantidad de ejes y de rodadas. Modele este sistema con diagramas de clase UML 
+indicando las relaciones de herencia y codifique un sistema mediante el cual se 
+genere instancia de las clases auto y camioneta, donde el usuario captura de 
+sus datos e inserte los respectivos objetos.*/
 <!DOCTYPE html>
 <html lang="en">
 <head>
